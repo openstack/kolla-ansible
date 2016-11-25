14f9246fcdcd7c9428d3e837eeb66a06adb68bce
Vladislav Belogrudov <vladislav.belogrudov@oracle.com>
2016-11-25 15:52:03 +0300

    Add tool to merge passwords during release upgrade
    Typical usage:
       cp /etc/kolla/passwords.yml /etc/kolla/passwords.yml.old
       kolla-genpwd
       kolla-mergepwd --old /etc/kolla/passwords.yml.old \
                      --new /etc/kolla/passwords.yml     \
                      --final /etc/kolla/passwords.yml
    
    Change-Id: Ibbc598909e28e096145841aec929d0cfcf8f7cab
    Implements: blueprint kolla-merge-passwords

---
+++ kolla/cmd/mergepwd.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import yaml
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--old", help="old password file", required=True)
+    parser.add_argument("--new", help="new password file", required=True)
+    parser.add_argument("--final", help="merged password file", required=True)
+    args = parser.parse_args()
+
+    with open(args.old, "r") as old_file:
+        old_passwords = yaml.safe_load(old_file)
+
+    with open(args.new, "r") as new_file:
+        new_passwords = yaml.safe_load(new_file)
+
+    new_passwords.update(old_passwords)
+
+    with open(args.final, "w") as destination:
+        yaml.dump(new_passwords, destination)
+
+
+if __name__ == '__main__':
+    main()
