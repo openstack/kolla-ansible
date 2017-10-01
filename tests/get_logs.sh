b2c0830006a63bc351f345577dac595c2207f3ab
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2017-10-01 23:21:38 +0800

    Move to zuul v3 in project jobs
    - move check container failure from post.yml to run.yml
    - add binary related jobs
    - use static kolla-ansible src dir, which is helpful for kolla project
      to use.
    - generate correct /etc/hosts by using private ip address and hostname
    - fix the wrong api interface in global.yml file
    
    Partial-Bug: #1720601
    Change-Id: Ibc20a6ae8c645ff82f3c14a6286073dffd4cfae2
    (cherry picked from commit 3908934f70c4c4cd7a4b990e9b3f2881ae40b3cb)

--- tests/get_logs.sh
+++ tests/get_logs.sh
@@ -1,43 +1,68 @@
 #!/bin/bash
 
+set +o errexit
+
 copy_logs() {
-    cp -rnL /var/lib/docker/volumes/kolla_logs/_data/* /tmp/logs/kolla/
-    cp -rnL /etc/kolla/* /tmp/logs/kolla_configs/
-    cp -rvnL /var/log/* /tmp/logs/system_logs/
+    LOG_DIR=/tmp/logs
+
+    cp -rnL /var/lib/docker/volumes/kolla_logs/_data/* ${LOG_DIR}/kolla/
+    cp -rnL /etc/kolla/* ${LOG_DIR}/kolla_configs/
+    cp -rvnL /var/log/* ${LOG_DIR}/system_logs/
 
 
     if [[ -x "$(command -v journalctl)" ]]; then
-        journalctl --no-pager -u docker.service > /tmp/logs/system_logs/docker.log
+        journalctl --no-pager > ${LOG_DIR}/system_logs/syslog.txt
+        journalctl --no-pager -u docker.service > ${LOG_DIR}/system_logs/docker.log
     else
-        cp /var/log/upstart/docker.log /tmp/logs/system_logs/docker.log
+        cp /var/log/upstart/docker.log ${LOG_DIR}/system_logs/docker.log
     fi
-}
 
-check_failure() {
-    # Command failures after this point can be expected
-    set +o errexit
+    cp -r /etc/sudoers.d ${LOG_DIR}/system_logs/
+    cp /etc/sudoers ${LOG_DIR}/system_logs/sudoers.txt
 
-    docker images
-    docker ps -a
-    # All docker container's status are created, restarting, running, removing,
-    # paused, exited and dead. Containers without running status are treated as
-    # failure. removing is added in docker 1.13, just ignore it now.
-    failed_containers=$(docker ps -a --format "{{.Names}}" \
-        --filter status=created \
-        --filter status=restarting \
-        --filter status=paused \
-        --filter status=exited \
-        --filter status=dead)
+    df -h > ${LOG_DIR}/system_logs/df.txt
+    free  > ${LOG_DIR}/system_logs/free.txt
+    parted -l > ${LOG_DIR}/system_logs/parted-l.txt
+    mount > ${LOG_DIR}/system_logs/mount.txt
+    env > ${LOG_DIR}/system_logs/env.txt
 
-    for failed in ${failed_containers}; do
-        docker logs --tail all ${failed} > /tmp/logs/docker_logs/${failed}
+    if [ `command -v dpkg` ]; then
+        dpkg -l > ${LOG_DIR}/system_logs/dpkg-l.txt
+    fi
+    if [ `command -v rpm` ]; then
+        rpm -qa > ${LOG_DIR}/system_logs/rpm-qa.txt
+    fi
+
+    # final memory usage and process list
+    ps -eo user,pid,ppid,lwp,%cpu,%mem,size,rss,cmd > ${LOG_DIR}/system_logs/ps.txt
+
+    # docker related information
+    (docker info && docker images && docker ps -a) > ${LOG_DIR}/system_logs/docker-info.txt
+
+    # ceph related logs
+    if [[ $(docker ps --filter name=ceph_mon --format "{{.Names}}") ]]; then
+        docker exec ceph_mon ceph -s > ${LOG_DIR}/kolla/ceph/ceph_s.txt
+        docker exec ceph_mon ceph osd df > ${LOG_DIR}/kolla/ceph/ceph_osd_df.txt
+        docker exec ceph_mon ceph osd tree > ${LOG_DIR}/kolla/ceph/ceph_osd_tree.txt
+    fi
+
+    for container in $(docker ps -a --format "{{.Names}}"); do
+        docker logs --tail all ${container} > ${LOG_DIR}/docker_logs/${container}.txt
     done
 
-    copy_logs
+    # Rename files to .txt; this is so that when displayed via
+    # logs.openstack.org clicking results in the browser shows the
+    # files, rather than trying to send it to another app or make you
+    # download it, etc.
 
-    if [[ -n "$failed_containers" ]]; then
-        exit 1;
-    fi
+    # Rename all .log files to .txt files
+    for f in $(find ${LOG_DIR}/{system_logs,kolla,docker_logs} -name "*.log"); do
+        mv $f ${f/.log/.txt}
+    done
+
+    chmod -R 777 ${LOG_DIR}
+    find ${LOG_DIR}/{system_logs,kolla,docker_logs} -iname '*.txt' -execdir gzip -f -9 {} \+
+    find ${LOG_DIR}/{system_logs,kolla,docker_logs} -iname '*.json' -execdir gzip -f -9 {} \+
 }
 
-check_failure
+copy_logs
