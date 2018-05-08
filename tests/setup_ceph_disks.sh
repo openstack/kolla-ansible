3591d0fa9f3211bcc707bb0fc652a8fb33545dcb
Tone Zhang <tone.zhang@arm.com>
2018-05-08 07:27:56 +0000

    Allow Kolla Ceph to deploy bluestore OSDs in Kolla-ansible
    Support Kolla Ceph to deploy blustore OSDs with Kolla-ansible.
    
    Please refer to [1] for bluestore OSD configuration
    
    The patch includes:
    1. Set Ceph OSD store type group_vars/all.yml. The default value
    is "bluestore" in Rocky.
    
    2. Make Kolla Ceph to deploy bluestore OSDs with Kolla-ansible
    
    3. Update gate test configuration for Ceph bluestore OSD test
    
    [1]: specs/kolla-ceph-bluestore.rst
    
    Partially-Implements: blueprint kolla-ceph-bluestore
    Depends-On: I00eaa600a5e9ad4c1ebca2eeb523bca3d7a25128
    Change-Id: I14f20a00654dff32c36d078ebb9005d91a3e60b2
    Signed-off-by: Tone Zhang <tone.zhang@arm.com>

--- tests/setup_ceph_disks.sh
+++ tests/setup_ceph_disks.sh
@@ -1,13 +1,23 @@
 mkdir -p /opt/data/kolla
-dd if=/dev/zero of=/opt/data/kolla/ceph-osd0.img bs=5M count=2048
+dd if=/dev/zero of=/opt/data/kolla/ceph-osd0.img bs=5M count=128
 LOOP=$(losetup -f)
 losetup $LOOP /opt/data/kolla/ceph-osd0.img
-parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_OSD1 1 -1
+parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_OSD1 1 -1
 
-dd if=/dev/zero of=/opt/data/kolla/ceph-journal0.img bs=5M count=512
+dd if=/dev/zero of=/opt/data/kolla/ceph-osd0-b.img bs=5M count=2048
 LOOP=$(losetup -f)
-losetup $LOOP /opt/data/kolla/ceph-journal0.img
-parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_OSD1_J 1 -1
+losetup $LOOP /opt/data/kolla/ceph-osd0-b.img
+parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_B_OSD1 1 -1
 
-partprobe $LOOP
+dd if=/dev/zero of=/opt/data/kolla/ceph-osd0-w.img bs=5M count=256
+LOOP=$(losetup -f)
+losetup $LOOP /opt/data/kolla/ceph-osd0-w.img
+parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_W_OSD1 1 -1
+
+dd if=/dev/zero of=/opt/data/kolla/ceph-osd0-d.img bs=5M count=256
+LOOP=$(losetup -f)
+losetup $LOOP /opt/data/kolla/ceph-osd0-d.img
+parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_D_OSD1 1 -1
+
+partprobe
 
