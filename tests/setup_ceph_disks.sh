bc70d016f61db3a1bf2c82bd0da58844365d6b6a
wangwei <wangwei@navercorp.com>
2018-08-27 16:00:42 +0900

    Fix bluestore disk naming format in kolla-ansible
    The current bluestore disk label naming is inconsistent with the
    filestore. The filestore naming format is that the disk prefixes
    belonging to the same osd are the same and the suffixes are
    different.
    
    This patch keeps the bluestore's disk naming as well.
    
    Change-Id: I71dda29fc4a6765300ce7bb173d2c448c24f6eca
    (cherry picked from commit 4e5e28fff53abf526042c965f6770c28c7c37742)

--- tests/setup_ceph_disks.sh
+++ tests/setup_ceph_disks.sh
@@ -7,17 +7,17 @@ parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_OSD1 1 -1
 dd if=/dev/zero of=/opt/data/kolla/ceph-osd0-b.img bs=5M count=2048
 LOOP=$(losetup -f)
 losetup $LOOP /opt/data/kolla/ceph-osd0-b.img
-parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_B_OSD1 1 -1
+parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_OSD1_B 1 -1
 
 dd if=/dev/zero of=/opt/data/kolla/ceph-osd0-w.img bs=5M count=256
 LOOP=$(losetup -f)
 losetup $LOOP /opt/data/kolla/ceph-osd0-w.img
-parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_W_OSD1 1 -1
+parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_OSD1_W 1 -1
 
 dd if=/dev/zero of=/opt/data/kolla/ceph-osd0-d.img bs=5M count=256
 LOOP=$(losetup -f)
 losetup $LOOP /opt/data/kolla/ceph-osd0-d.img
-parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_D_OSD1 1 -1
+parted $LOOP -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP_BS_OSD1_D 1 -1
 
 partprobe
 
