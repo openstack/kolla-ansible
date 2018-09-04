fcc4b5d0e1ade66df2df6ddc36fcbcf99ffc92ec
Mark Goddard <mark@stackhpc.com>
2018-09-04 15:17:14 +0100

    Add check.yml for vitrage
    Without this, kolla-ansible check fails with the following error:
    
    Unable to retrieve file contents
    Could not find or access '/path/to/kolla-ansible/ansible/check.yml'"}
    
    Also adds the check command to the CI tests, to ensure that it does not
    break again.
    
    Change-Id: I9fc2f9999f55cb742ac3ac38579dcf26524a9fc7
    Closes-Bug: #1790653
    (cherry picked from commit 354894e2e9ca887d04a18d2843cd0492d9981e4a)

--- tools/setup_gate.sh
+++ tools/setup_gate.sh
@@ -174,6 +174,7 @@ tools/kolla-ansible -i ${RAW_INVENTORY} -vvv prechecks > /tmp/logs/ansible/prech
 # service in CI
 tools/kolla-ansible -i ${RAW_INVENTORY} -vvv deploy > /tmp/logs/ansible/deploy
 tools/kolla-ansible -i ${RAW_INVENTORY} -vvv post-deploy > /tmp/logs/ansible/post-deploy
+tools/kolla-ansible -i ${RAW_INVENTORY} -vvv check > /tmp/logs/ansible/check-deploy
 
 # Test OpenStack Environment
 # TODO: use kolla-ansible check when it's ready
@@ -183,8 +184,10 @@ sanity_check
 # TODO(jeffrey4l): make some configure file change and
 # trigger a real reconfigure
 tools/kolla-ansible -i ${RAW_INVENTORY} -vvv reconfigure >  /tmp/logs/ansible/post-deploy
+tools/kolla-ansible -i ${RAW_INVENTORY} -vvv check > /tmp/logs/ansible/check-reconfigure
 # TODO(jeffrey4l): need run a real upgrade
 tools/kolla-ansible -i ${RAW_INVENTORY} -vvv upgrade > /tmp/logs/ansible/upgrade
+tools/kolla-ansible -i ${RAW_INVENTORY} -vvv check > /tmp/logs/ansible/check-upgrade
 
 # run prechecks again
 tools/kolla-ansible -i ${RAW_INVENTORY} -vvv prechecks > /tmp/logs/ansible/prechecks2
