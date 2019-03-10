ce533592e41950bf6207e8b854e7032983d6f3f8
Victor Coutellier <victor.coutellier@gmail.com>
2019-03-10 19:07:00 +0100

    Fix kolla-docker possible undefined variable
    It is possible to reference undefined variable in kolla-docker module if
    DockerWorker object initialization fail, so the current behaviour will
    crash the playbook with the unwanted error message :
    
    UnboundLocalError: local variable 'dw' referenced before assignment
    
    Change-Id: Ic8d26b11f93255220888b5406f8ab4a6f81736c2
    Closes-Bug: #1819361
    (cherry picked from commit 3bb4c199817c8aa4ecd4ae684bd28f84933fb14d)

--- ansible/library/kolla_docker.py
+++ ansible/library/kolla_docker.py
@@ -901,6 +901,7 @@ def generate_module():
 def main():
     module = generate_module()
 
+    dw = None
     try:
         dw = DockerWorker(module)
         # TODO(inc0): We keep it bool to have ansible deal with consistent
@@ -910,7 +911,7 @@ def main():
         module.exit_json(changed=dw.changed, result=result, **dw.result)
     except Exception:
         module.fail_json(changed=True, msg=repr(traceback.format_exc()),
-                         **dw.result)
+                         **getattr(dw, 'result', {}))
 
 # import module snippets
 from ansible.module_utils.basic import *  # noqa
