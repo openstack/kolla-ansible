b9d6cc2a7f2969af9bbef751db5899edce7eb1c2
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2019-10-02 03:53:48 -0700

    Add Debian/source CI job
    Change-Id: I0628b16e3ebdb3fa8196acdc1bd9c63e75bcfb09

--- tools/setup_gate.sh
+++ tools/setup_gate.sh
@@ -25,18 +25,6 @@ function setup_openstack_clients {
 }
 
 function setup_config {
-    # Use Infra provided pypi.
-    # Wheel package mirror may be not compatible. So do not enable it.
-    PIP_CONF=$(mktemp)
-    cat > ${PIP_CONF} <<EOF
-[global]
-timeout = 60
-index-url = $NODEPOOL_PYPI_MIRROR
-trusted-host = $NODEPOOL_MIRROR_HOST
-EOF
-    echo "RUN echo $(base64 -w0 ${PIP_CONF}) | base64 -d > /etc/pip.conf" | sudo tee /etc/kolla/header
-    rm ${PIP_CONF}
-
     if [[ $ACTION != "bifrost" ]]; then
         GATE_IMAGES="cron,fluentd,glance,haproxy,keepalived,keystone,kolla-toolbox,mariadb,memcached,neutron,nova,openvswitch,rabbitmq,horizon,chrony,heat,placement"
     else
@@ -67,7 +55,6 @@ EOF
 
     cat <<EOF | sudo tee /etc/kolla/kolla-build.conf
 [DEFAULT]
-include_header = /etc/kolla/header
 namespace = lokolla
 base = ${BASE_DISTRO}
 install_type = ${INSTALL_TYPE}
@@ -83,29 +70,6 @@ gate = ${GATE_IMAGES}
 EOF
 
     mkdir -p /tmp/logs/build
-
-    if [[ "${DISTRO}" == "Debian" ]]; then
-        # Infra does not sign their mirrors so we ignore gpg signing in the gate
-        echo "RUN echo 'APT::Get::AllowUnauthenticated \"true\";' > /etc/apt/apt.conf" | sudo tee -a /etc/kolla/header
-
-        # Optimize the repos to take advantage of the Infra provided mirrors for Ubuntu
-        cat << EOF | sudo tee -a /etc/kolla/kolla-build.conf
-apt_sources_list = /etc/kolla/sources.list
-EOF
-        sudo cp /etc/apt/sources.list /etc/kolla/sources.list
-        sudo cat /etc/apt/sources.list.available.d/ubuntu-cloud-archive-pike.list | sudo tee -a /etc/kolla/sources.list
-        # Append non-infra provided repos to list
-        cat << EOF | sudo tee -a /etc/kolla/sources.list
-deb http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu xenial main
-deb http://repo.percona.com/apt xenial main
-deb http://packages.elastic.co/elasticsearch/2.x/debian stable main
-deb http://packages.elastic.co/kibana/4.6/debian stable main
-EOF
-    fi
-}
-
-function detect_distro {
-    DISTRO=$(ansible all -i "localhost," -msetup -clocal | awk -F\" '/ansible_os_family/ {print $4}')
 }
 
 function setup_ansible {
@@ -120,8 +84,6 @@ function setup_ansible {
 
     # TODO(SamYaple): Move to virtualenv
     sudo pip install -U "ansible${ANSIBLE_VERSION}" "ara<1.0.0"
-
-    detect_distro
 
     sudo mkdir /etc/ansible
     ara_location=$(python -m ara.setup.callback_plugins)
