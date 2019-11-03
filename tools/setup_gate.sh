be87558acfb8df3831aba94a3abddd5236dcb6f6
Radosław Piliszek <radoslaw.piliszek@gmail.com>
2019-11-03 16:26:01 +0100

    CI: Pin Ansible to 2.8.x on Ubuntu during upgrades
    Stein-only
    
    Ansible 2.9 removed version_compare which Rocky relied on.
    
    See https://review.opendev.org/692575 for change in Rocky.
    
    Change-Id: I393d90396730bdfafe66ff473528bd30dfe63c21

--- tools/setup_gate.sh
+++ tools/setup_gate.sh
@@ -108,6 +108,12 @@ function setup_ansible {
     # Test latest ansible version on Ubuntu, minimum supported on others.
     if [[ $BASE_DISTRO == "ubuntu" ]]; then
         ANSIBLE_VERSION=">=2.5"
+        # When upgrading from Rocky and earlier, we have to limit the version
+        # due to version_compare being gone from Ansible 2.9
+        # see https://review.opendev.org/692575 for change in Rocky
+        if [[ $ACTION =~ "upgrade" ]]; then
+            ANSIBLE_VERSION="$ANSIBLE_VERSION,<2.9"
+        fi
     else
         ANSIBLE_VERSION="<2.6"
     fi
