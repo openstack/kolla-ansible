4beb760d8d72c8bed71a224813e64682dd4d688e
Radosław Piliszek <radoslaw.piliszek@gmail.com>
2019-11-20 14:25:16 +0100

    CI: Refactor a lot
    Separate upgrade logic to is_upgrade job var and rename
    scenarios to match.
    
    Rename "ACTION" to "SCENARIO" (as it is a scenario).
    
    Separate testing of dashboard (aka Horizon) and increase
    its timeout to 5 minutes (CentOS 7 slow as always).
    
    Separate initialization of core OpenStack.
    
    Use gate setup script from ./tests/
    
    Remove useless tox setupenv.
    
    Do not deploy Heat when not really necessary.
    
    Change-Id: I4fca319ccc3de7188f8b7b44c9c71321e3899467
    (cherry picked from commit a2fc684164cb5169492d6e0c007ad006f814492c)

--- tests/test-core-openstack.sh
+++ tests/test-core-openstack.sh
@@ -11,7 +11,7 @@ function test_smoke {
     openstack --debug compute service list
     openstack --debug network agent list
     openstack --debug orchestration service list
-    if [[ $ACTION =~ "ceph" ]] || [[ $ACTION == "cinder-lvm" ]]; then
+    if [[ $SCENARIO == "ceph" ]] || [[ $SCENARIO == "cinder-lvm" ]]; then
         openstack --debug volume service list
     fi
 }
@@ -28,7 +28,7 @@ function test_instance_boot {
     fi
     echo "SUCCESS: Server creation"
 
-    if [[ $ACTION =~ "ceph" ]] || [[ $ACTION == "cinder-lvm" ]]; then
+    if [[ $SCENARIO == "ceph" ]] || [[ $SCENARIO == "cinder-lvm" ]]; then
         echo "TESTING: Cinder volume attachment"
         openstack volume create --size 2 test_volume
         attempt=1
@@ -75,58 +75,25 @@ function test_instance_boot {
     echo "SUCCESS: Server deletion"
 }
 
-function check_dashboard {
-    # Query the dashboard, and check that the returned page looks like a login
-    # page.
-    DASHBOARD_URL=${OS_AUTH_URL%:*}
-    output_path=$1
-    if ! curl --include --location --fail $DASHBOARD_URL > $output_path; then
-        return 1
-    fi
-    if ! grep Login $output_path >/dev/null; then
-        return 1
-    fi
-}
-
-function test_dashboard {
-    echo "TESTING: Dashboard"
-    # The dashboard has been known to take some time to become accessible, so
-    # use retries.
-    output_path=$(mktemp)
-    attempt=1
-    while ! check_dashboard $output_path; do
-        echo "Dashboard not accessible yet"
-        attempt=$((attempt+1))
-        if [[ $attempt -eq 10 ]]; then
-            echo "FAILED: Dashboard did not become accessible. Response:"
-            cat $output_path
-            return 1
-        fi
-        sleep 10
-    done
-    echo "SUCCESS: Dashboard"
-}
-
 function test_openstack_logged {
     . /etc/kolla/admin-openrc.sh
     . ~/openstackclient-venv/bin/activate
     test_smoke
     test_instance_boot
-    test_dashboard
 }
 
 function test_openstack {
     echo "Testing OpenStack"
-    log_file=/tmp/logs/ansible/test-openstack
+    log_file=/tmp/logs/ansible/test-core-openstack
     if [[ -f $log_file ]]; then
         log_file=${log_file}-upgrade
     fi
     test_openstack_logged > $log_file 2>&1
     result=$?
     if [[ $result != 0 ]]; then
-        echo "Testing OpenStack failed. See ansible/test-openstack for details"
+        echo "Testing OpenStack failed. See ansible/test-core-openstack for details"
     else
-        echo "Successfully tested OpenStack. See ansible/test-openstack for details"
+        echo "Successfully tested OpenStack. See ansible/test-core-openstack for details"
     fi
     return $result
 }
