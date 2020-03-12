7367427338ee4f51624742f882d7780000238ef3
Radosław Piliszek <radoslaw.piliszek@gmail.com>
2020-03-12 19:50:16 +0100

    [CI] Test Neutron with IPv6 networks
    Make sure to actually test IPv6 connectivity to our test instance in the
    IPv6 jobs.
    
    Change-Id: I7845448804e191af356e82f8ad33c563ffd8ebd5

--- tests/init-core-openstack.sh
+++ tests/init-core-openstack.sh
@@ -13,6 +13,12 @@ function init_runonce {
 
     echo "Initialising OpenStack resources via init-runonce"
     KOLLA_DEBUG=1 tools/init-runonce |& gawk '{ print strftime("%F %T"), $0; }' &> /tmp/logs/ansible/init-runonce
+
+    if [[ $IP_VERSION -eq 6 ]]; then
+        # NOTE(yoctozepto): In case of IPv6 there is no NAT support in Neutron,
+        # so we have to set up native routing. Static routes are the simplest.
+        sudo ip route add ${DEMO_NET_CIDR} via ${EXT_NET_DEMO_ROUTER_ADDR}
+    fi
 }
 
 
