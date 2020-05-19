96a5730a429c3eba5d0f2f98d0ad0bae4cefcec9
Radosław Piliszek <radoslaw.piliszek@gmail.com>
2020-05-19 20:08:46 +0200

    Fix pygments style
    New theme of docs (Victoria+) respects pygments_style.
    Since Kolla starts using Victoria reqs while being on Ussuri,
    this patch ensures proper rendering both in Ussuri and Victoria.
    Thanks @AJaeger for suggestion.
    
    Change-Id: Iaf3c70b24685ab962f29007deec10b9d53c663bc

--- deploy-guide/source/conf.py
+++ deploy-guide/source/conf.py
@@ -107,7 +107,7 @@ exclude_patterns = ["common_prerequisites.rst", "common_configure.rst"]
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
