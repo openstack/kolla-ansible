be7d640c05c34cd01d1f364e6b028607e93cc032
wu.chunyang <wuchunyang@yovole.com>
2020-11-18 22:53:04 +0800

    [CI] Add trove smoke test CI
    Add trove smoke test.
    
    Change-Id: I571d9663bcadb1ddc5a7ad86a2c478706e0705a9

--- tests/setup_gate.sh
+++ tests/setup_gate.sh
@@ -18,7 +18,7 @@ function setup_openstack_clients {
         packages+=(python-ironicclient python-ironic-inspector-client)
     fi
     if [[ $SCENARIO == magnum ]]; then
-        packages+=(python-designateclient python-magnumclient python-octaviaclient)
+        packages+=(python-designateclient python-magnumclient python-octaviaclient python-troveclient)
     fi
     if [[ $SCENARIO == masakari ]]; then
         packages+=(python-masakariclient)
@@ -63,7 +63,7 @@ function prepare_images {
         GATE_IMAGES+=",^dnsmasq,^ironic,^iscsid"
     fi
     if [[ $SCENARIO == "magnum" ]]; then
-        GATE_IMAGES+=",^designate,^magnum,^octavia"
+        GATE_IMAGES+=",^designate,^magnum,^octavia,^trove"
     fi
     if [[ $SCENARIO == "masakari" ]]; then
         GATE_IMAGES+=",^masakari"
