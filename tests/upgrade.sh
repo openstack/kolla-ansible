4920980add9f978499770e7ec9afcdc7e8a15ec2
Mark Goddard <mark@stackhpc.com>
2021-02-12 17:58:51 +0000

    CI: Use PATH to find kolla-ansible script
    This change also updates the CI test scripts to use PATH to find the
    kolla-ansible script, rather than relying on the file in the source
    checkout.
    
    Using the script in the source checkout was hiding an issue with pip
    install --user, although that has now been fixed in
    I5b47a146627d06bb3fe4a747c5f20290c726b0f9.
    
    Related-Bug: #1915527
    
    Change-Id: I2827a657c8716a9c40391c6bdb7ff1a2a9c1260e
    Depends-On: https://review.opendev.org/c/openstack/kolla-ansible/+/793570
    (cherry picked from commit 1ea99147c14068b7edbaf36a23199bc93a5ef493)

--- tests/upgrade.sh
+++ tests/upgrade.sh
@@ -10,10 +10,10 @@ export PYTHONUNBUFFERED=1
 function upgrade {
     RAW_INVENTORY=/etc/kolla/inventory
 
-    tools/kolla-ansible -i ${RAW_INVENTORY} -vvv prechecks &> /tmp/logs/ansible/upgrade-prechecks
-    tools/kolla-ansible -i ${RAW_INVENTORY} -vvv pull &> /tmp/logs/ansible/pull-upgrade
-    tools/kolla-ansible -i ${RAW_INVENTORY} -vvv upgrade &> /tmp/logs/ansible/upgrade
-    tools/kolla-ansible -i ${RAW_INVENTORY} -vvv check &> /tmp/logs/ansible/check-upgrade
+    kolla-ansible -i ${RAW_INVENTORY} -vvv prechecks &> /tmp/logs/ansible/upgrade-prechecks
+    kolla-ansible -i ${RAW_INVENTORY} -vvv pull &> /tmp/logs/ansible/pull-upgrade
+    kolla-ansible -i ${RAW_INVENTORY} -vvv upgrade &> /tmp/logs/ansible/upgrade
+    kolla-ansible -i ${RAW_INVENTORY} -vvv check &> /tmp/logs/ansible/check-upgrade
 }
 
 
