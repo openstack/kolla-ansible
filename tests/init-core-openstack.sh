337771143ba7c392665a14e8aa2434817d9e9473
Michał Nasiadka <mnasiadka@gmail.com>
2021-05-20 12:22:27 +0200

    CI: Configure IP on a linux bridge instead of OVS br-ex
    OVS upgrade jobs failed due to assigning an IP on the br-ex interface
    (which is recycled during OVS upgrade). This change introduces a bridge
    and veth pair for Neutron to use.
    
    Change-Id: Ib3bee6e810fb8d31552d4c72c2a1ccae382c51f0

--- tests/init-core-openstack.sh
+++ tests/init-core-openstack.sh
@@ -13,19 +13,6 @@ function init_runonce {
 
     echo "Initialising OpenStack resources via init-runonce"
     KOLLA_DEBUG=1 tools/init-runonce |& gawk '{ print strftime("%F %T"), $0; }' &> /tmp/logs/ansible/init-runonce
-
-    echo "Setting address on the external network bridge"
-    if [[ $SCENARIO == "linuxbridge" ]]; then
-        # NOTE(yoctozepto): linuxbridge agent manages its bridges by itself
-        # hence, we need to find the current name of the external network bridge
-        devname=$(basename $(readlink /sys/class/net/${EXT_NET_SLAVE_DEVICE}/master))
-    else
-        devname=br-ex
-        # NOTE(yoctozepto): ovs virtual interfaces are down (not used) by default
-        # hence, we need to bring the external network bridge up
-        sudo ip link set ${devname} up
-    fi
-    sudo ip addr add ${EXT_NET_LOCAL_ADDR} dev ${devname}
 }
 
 
