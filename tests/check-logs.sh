7ccea278279fa70a98c227a3a8c994feb94b40b4
Radosław Piliszek <radoslaw.piliszek@gmail.com>
2022-05-06 15:34:55 +0200

    [CI] Raise [keystone_authtoken]http_request_max_retries
    This is in the hope to avoid random "Unable to validate token" in
    AIO upgrade jobs which restart the Keystone container while
    other services are trying to use it (most notably Placement but
    also Nova and Neutron).
    
    Change-Id: Ie6a95c3ca84df82ca8463cf76054e125cb1ffff1

--- tests/check-logs.sh
+++ tests/check-logs.sh
@@ -28,15 +28,10 @@ function filter_out_expected_critical {
     # job.
 
     case $1 in
-    */placement-api.log)
-        # Sometimes we see this during upgrade when keystone is down.
-        grep -v "Failed to fetch token data from identity server"
-        ;;
     */neutron-server.log)
         # Sometimes we see this during shutdown (upgrade).
         # See: https://bugs.launchpad.net/neutron/+bug/1863579
         grep -v "WSREP has not yet prepared node for application use"
-        grep -v "Failed to fetch token data from identity server"
         ;;
     *)
         # We have to provide some pass-through consumer to avoid:
