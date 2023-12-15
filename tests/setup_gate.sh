449a98c5a3a5a91f6e67a1c651ec8473b654c1a7
Michal Nasiadka <mnasiadka@gmail.com>
2023-12-15 17:41:09 +0100

    CI: Template out kolla-build.conf using Ansible role
    Change-Id: I79ec11c013fb1d7381fc152ab92e3d607ae822b9
    Signed-off-by: Michal Nasiadka <mnasiadka@gmail.com>
    (cherry picked from commit 9cee57a4c4f8b30b4b7b1223718ee20dbde6eab6)

--- tests/setup_gate.sh
+++ tests/setup_gate.sh
@@ -12,87 +12,6 @@ function prepare_images {
         return
     fi
 
-    if [[ $SCENARIO != "bifrost" ]]; then
-        GATE_IMAGES="^cron,^fluentd,^glance,^haproxy,^proxysql,^keepalived,^keystone,^kolla-toolbox,^mariadb,^memcached,^neutron,^nova-,^openvswitch,^rabbitmq,^horizon,^heat,^placement"
-    else
-        GATE_IMAGES="bifrost"
-    fi
-
-    if [[ $SCENARIO == "cephadm" ]]; then
-        GATE_IMAGES+=",^cinder,^redis"
-    fi
-
-    if [[ $SCENARIO == "cells" ]]; then
-        GATE_IMAGES+=",^proxysql"
-    fi
-
-    if [[ $SCENARIO == "zun" ]]; then
-        GATE_IMAGES+=",^zun,^kuryr,^etcd,^cinder,^iscsid"
-        if [[ $BASE_DISTRO != "centos" && $BASE_DISTRO != "rocky" ]]; then
-            GATE_IMAGES+=",^tgtd"
-        fi
-    fi
-
-    if [[ $SCENARIO == "scenario_nfv" ]]; then
-        GATE_IMAGES+=",^aodh,^tacker,^mistral,^redis,^barbican"
-    fi
-    if [[ $SCENARIO == "ironic" ]]; then
-        GATE_IMAGES+=",^dnsmasq,^ironic,^iscsid,^prometheus"
-    fi
-    if [[ $SCENARIO == "magnum" ]]; then
-        GATE_IMAGES+=",^designate,^magnum,^trove"
-    fi
-    if [[ $SCENARIO == "octavia" ]]; then
-        GATE_IMAGES+=",^redis,^octavia"
-    fi
-    if [[ $SCENARIO == "masakari" ]]; then
-        GATE_IMAGES+=",^masakari-,^hacluster-"
-    fi
-
-    if [[ $SCENARIO == "ovn" ]]; then
-        GATE_IMAGES+=",^redis,^octavia,^ovn"
-    fi
-
-    if [[ $SCENARIO == "mariadb" ]]; then
-        GATE_IMAGES="^cron,^fluentd,^haproxy,^proxysql,^keepalived,^kolla-toolbox,^mariadb"
-    fi
-
-    if [[ $SCENARIO == "lets-encrypt" ]]; then
-        GATE_IMAGES+=",^letsencrypt,^haproxy"
-    fi
-
-    if [[ $SCENARIO == "prometheus-opensearch" ]]; then
-        GATE_IMAGES="^cron,^fluentd,^grafana,^haproxy,^proxysql,^keepalived,^kolla-toolbox,^mariadb,^memcached,^opensearch,^prometheus,^rabbitmq"
-    fi
-
-    if [[ $SCENARIO == "ipv6" ]]; then
-        GATE_IMAGES+=",^prometheus"
-    fi
-
-    if [[ $SCENARIO == "venus" ]]; then
-        GATE_IMAGES="^cron,^opensearch,^fluentd,^haproxy,^proxysql,^keepalived,^keystone,^kolla-toolbox,^mariadb,^memcached,^rabbitmq,^venus"
-    fi
-
-    if [[ $SCENARIO == "skyline" || $SCENARIO == "skyline-sso" ]]; then
-        GATE_IMAGES+=",^skyline"
-    fi
-
-    sudo tee -a /etc/kolla/kolla-build.conf <<EOF
-[DEFAULT]
-engine = ${CONTAINER_ENGINE}
-EOF
-
-    if [[ $BASE_DISTRO == "debian" || $BASE_DISTRO == "ubuntu" ]]; then
-        sudo tee -a /etc/kolla/kolla-build.conf <<EOF
-base_image = quay.io/openstack.kolla/${BASE_DISTRO}
-EOF
-    fi
-
-    sudo tee -a /etc/kolla/kolla-build.conf <<EOF
-[profiles]
-gate = ${GATE_IMAGES}
-EOF
-
     sudo mkdir -p /tmp/logs/build
     sudo mkdir -p /opt/kolla_registry
 
@@ -108,14 +27,6 @@ EOF
     fi
 
     sudo ~/kolla-venv/bin/kolla-build
-
-    # NOTE(yoctozepto): due to debian buster we push after images are built
-    # see https://github.com/docker/for-linux/issues/711
-    if [[ "debian" == $BASE_DISTRO ]]; then
-        for img in $(sudo ${CONTAINER_ENGINE} image ls --format '{{ .Repository }}:{{ .Tag }}' | grep lokolla/); do
-            sudo $CONTAINER_ENGINE push $img;
-        done
-    fi
 
     deactivate
 }
