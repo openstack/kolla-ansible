3c3a18aa5b2f0228d489566bb255912c0f0e3892
Sven Kieske <kieske@osism.tech>
2024-08-29 13:51:18 +0200

    Rabbitmq: enable quorum for transient queues
    This helps to improve the reliability of openstack services when
    a rabbitmq node has issues.
    
    See also: https://bugs.launchpad.net/oslo.messaging/+bug/2031497
    
    Adjust the upgrade tests similar to what was done
    during the introduction of quorum queues in I6c033d460a5c9b93c346e9e47e93b159d3c27830
    
    Closes-Bug: #2078339
    Partial-Bug: #2077448
    Depends-On: https://review.opendev.org/c/openstack/oslo.messaging/+/888479
    Depends-On: https://review.opendev.org/c/openstack/kolla-ansible/+/924623
    Signed-off-by: Sven Kieske <kieske@osism.tech>
    Change-Id: Idb8a8d2e560206f7697c0771c9ae3913268fa6dd

--- tests/upgrade.sh
+++ tests/upgrade.sh
@@ -13,7 +13,38 @@ function upgrade {
     source $KOLLA_ANSIBLE_VENV_PATH/bin/activate
 
     kolla-ansible certificates -i ${RAW_INVENTORY} -vvv &> /tmp/logs/ansible/certificates
+    # Skip rabbitmq-ha-precheck before the queues are migrated.
+    kolla-ansible prechecks -i ${RAW_INVENTORY} --skip-tags rabbitmq-ha-precheck -vvv &> /tmp/logs/ansible/upgrade-prechecks-pre-rabbitmq
+
+    # NOTE(SvenKieske): As om_enable_rabbitmq_transient_quorum_queue now also
+    # enables quorum_queues for fanout/reply queues in Epoxy, we need
+    # to perform a migration to durable queues.
+    # TODO(SvenKieske): Remove these steps in F Cycle.
+    SERVICE_TAGS="heat,keystone,neutron,nova"
+    if [[ $SCENARIO == "zun" ]] || [[ $SCENARIO == "cephadm" ]]; then
+        SERVICE_TAGS+=",cinder"
+    fi
+    if [[ $SCENARIO == "scenario_nfv" ]]; then
+        SERVICE_TAGS+=",barbican"
+    fi
+    if [[ $SCENARIO == "ironic" ]]; then
+        SERVICE_TAGS+=",ironic"
+    fi
+    if [[ $SCENARIO == "masakari" ]]; then
+        SERVICE_TAGS+=",masakari"
+    fi
+    if [[ $SCENARIO == "ovn" ]] || [[ $SCENARIO == "octavia" ]]; then
+        SERVICE_TAGS+=",octavia"
+    fi
+    if [[ $SCENARIO == "magnum" ]]; then
+        SERVICE_TAGS+=",magnum,designate"
+    fi
+    kolla-ansible stop -i ${RAW_INVENTORY} -vvv --tags $SERVICE_TAGS --yes-i-really-really-mean-it --ignore-missing &> /tmp/logs/ansible/stop
+    kolla-ansible genconfig -i ${RAW_INVENTORY} -vvv &> /tmp/logs/ansible/genconfig
+    kolla-ansible rabbitmq-reset-state -i ${RAW_INVENTORY} -vvv &> /tmp/logs/ansible/rabbitmq-reset-state
+    # Include rabbitmq-ha-precheck this time to confirm all queues have migrated.
     kolla-ansible prechecks -i ${RAW_INVENTORY} -vvv &> /tmp/logs/ansible/upgrade-prechecks
+
     kolla-ansible pull -i ${RAW_INVENTORY} -vvv &> /tmp/logs/ansible/pull-upgrade
     kolla-ansible upgrade -i ${RAW_INVENTORY} -vvv &> /tmp/logs/ansible/upgrade
 
